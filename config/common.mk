e919947120a4badc454fb4da9add45c1af499109
Tim Schumacher <timschumi@gmx.de>
2018-06-20 06:23:53 +0200

    Install the Terminal by using the BUILD_PREBUILT target
    Previously (when the APK was simply copied over) the build
    system didn't know what to do with it when it came to
    signing.
    
    Change-Id: Ib4e4c8b94d3f84ecde520f0388582852503d99f5

--- config/common.mk
+++ config/common.mk
@@ -210,11 +210,9 @@ PRODUCT_PACKAGES += \
     Superuser \
     su
 
-# Terminal Emulator
-PRODUCT_COPY_FILES +=  \
-    vendor/crdroid/proprietary/Term.apk:system/app/Term.apk \
-    vendor/crdroid/proprietary/lib/armeabi/libjackpal-androidterm5.so:system/lib/libjackpal-androidterm5.so \
-    vendor/crdroid/proprietary/lib/armeabi/libjackpal-termexec2.so:system/lib/libjackpal-termexec2.so
+
+PRODUCT_PACKAGES += \
+    Terminal
 
 # AdAway
 PRODUCT_COPY_FILES += \
