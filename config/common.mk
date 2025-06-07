CyanogenMod/android_vendor_cm

terminal emulator: update libjackpal version
Change-Id: Ib5ddb4d5f42121c52884267730cc24c77f05b4a4

config/common.mk
@@ -219,7 +219,7 @@ PRODUCT_PACKAGES += \
 # Terminal Emulator
 PRODUCT_COPY_FILES +=  \
     vendor/cm/proprietary/Term.apk:system/app/Term.apk \
-    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
+    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm5.so:system/lib/libjackpal-androidterm5.so
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.root_access=1

crdroid-classic/android_vendor_crdroid

config/common.mk
@@ -213,7 +213,8 @@ PRODUCT_PACKAGES += \
 # Terminal Emulator
 PRODUCT_COPY_FILES +=  \
     vendor/crdroid/proprietary/Term.apk:system/app/Term.apk \
-    vendor/crdroid/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
+    vendor/crdroid/proprietary/lib/armeabi/libjackpal-androidterm5.so:system/lib/libjackpal-androidterm5.so \
+    vendor/crdroid/proprietary/lib/armeabi/libjackpal-termexec2.so:system/lib/libjackpal-termexec2.so
 
 # AdAway
 PRODUCT_COPY_FILES += \